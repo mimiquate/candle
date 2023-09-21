35b062c9cce74545e3c382367709e50fdc0b562f
Gonzalo <456459+grzuy@users.noreply.github.com>
2023-09-21 16:42:53 -0300

    fill_u8

--- candle-kernels/src/fill.cu
+++ candle-kernels/src/fill.cu
@@ -1,4 +1,5 @@
 #include "cuda_fp16.h"
+#include<stdint.h>
 
 template<typename T>
 __device__ void fill_with(T *buf, T value, const size_t numel) {
@@ -6,6 +7,7 @@ __device__ void fill_with(T *buf, T value, const size_t numel) {
         buf[i] = value;
     }
 }
+extern "C" __global__ void fill_u8(uint8_t *buf, uint8_t value, const size_t numel) { fill_with(buf, value, numel); }
 extern "C" __global__ void fill_f16(__half *buf, __half value, const size_t numel) { fill_with(buf, value, numel); }
 extern "C" __global__ void fill_f32(float *buf, float value, const size_t numel) { fill_with(buf, value, numel); }
 extern "C" __global__ void fill_f64(double *buf, double value, const size_t numel) { fill_with(buf, value, numel); }
